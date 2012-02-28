7d1350bc5c1be30fcd5d7e9e66e6aa700cc1cb21
Sakari Jokinen <sakariij@gmail.com>
2012-02-28 21:51:24 +0200

    shrinking

--- spec/javascripts/test/test.js
+++ spec/javascripts/test/test.js
@@ -9,14 +9,46 @@ describe('jsqc', function() {
 				 expect(values.length)
 				     .toBeGreaterThan(0);
 			     });
+
 			  it('throws when fails', function() {
 				 expect(function() {
 					    jsqc.property(jsqc.gen.integer, 
 							  function(){
 							      throw new Error();
 							  });
-					}).toThrow(new Error());
+					}).toThrow();
+			     });
+			  it('calls generator to produce test data', function() {
+				 function gen() {
+				     this.generate = function() {
+					 return "value";
+				     };
+				 };
+				 jsqc.property(gen, function(value) {
+						   expect(value)
+						       .toEqual("value");
+					       });
+			     });
+			  it('calls shrink to shrink failing test data', function() {
+				 function gen() {
+				     this.generate = function() {
+					 return 2;
+				     };
+				     this.shrink = function(value) {
+					 if (value === 2)
+					     return [1];
+				     };
+				 }
+				 var succeedingValue;
+				 expect(function() {
+					    jsqc.property(gen, function(value) {
+							      if(value > 1)
+								  throw new Error();
+							      succeedingValue = value;
+							  });})
+				     .toThrow();
+				 expect(succeedingValue).toEqual(1);
 			     });
 		      });
-
+	     
 	 });
--- src/jsqc.js
+++ src/jsqc.js
@@ -1,10 +1,40 @@
 jsqc = (function() {
+
+	    function minimize(generator, prop, starting_value) {
+		if(!generator.shrink)
+		    return starting_value;
+
+		var last_failing = starting_value;
+		var candidates = generator.shrink(starting_value);
+		for( var i in candidates ) {
+		    try {
+			prop(candidates[i]);
+		    } catch (x) {
+			last_failing = candidates[i];
+			candidates = generator.shrink(candidates[i]);
+		    }
+		}
+		return last_failing;
+	    }
+
 	    return {
 		gen : {
-		    integer : 0
+		    integer : function() {
+			this.generate = function() {return 0;};
+		    }
 		},
-		property : function(generator, prop) {
-		    prop(0);
+		property : function(gen, prop) {
+		    var generator = new gen();
+		    var value = generator.generate();
+		    try {
+			prop(value);
+		    } catch (x) {
+			value = minimize(generator, prop, value);
+			throw new Error('Failing case ' + 
+					value + 
+					' error:' + 
+					x);
+		    }
 		}
 	    };
 })();
