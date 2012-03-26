d0ef523008f04a25430929417b47d7561852237e
Sakari Jokinen <sakariij@gmail.com>
2012-03-26 23:49:07 +0300

    tidying test

--- spec/javascripts/test/jasmine.js
+++ spec/javascripts/test/jasmine.js
@@ -1,30 +1,64 @@
 describe('qc.jasmine', function() {
 	     describe('property', function() {
-			  var env;
+			  var _env, _ready, _suite;
 			  beforeEach(function() {
-					 env = new jasmine.Env();					 
+					 _ready = false;
+					 _suite = undefined;
+					 _env = new jasmine.Env();
 				     });
-			  it('is provided by jasmine', function(i) {
-				 var spec; 
-				 var suite = env.describe('suite', function() {
-							      env.property('property that holds', qc.gen.integer, 
-									   function(i) {
-									       this.expect(_.isNumber(i)).toEqual(true);
-									   });
-							      env.property('propert that fails with some value', qc.gen.integer,
-									  function(i) {
-									      this.expect(i).toBeLessThan(5);
-									  });
-							  });
-				 var ready;
-				 suite.execute(function() { ready = true; });
-				 waitsFor(function() { return ready; });
-				 runs(function() {
-					  console.log(suite.results());
-					  expect(suite.results().totalCount).toEqual(2);
-					  expect(suite.results().passedCount).toEqual(1);
-					  expect(suite.results().failedCount).toEqual(1);
-				      });
+			  function when_suite_has_been_run(fn) {
+			      _suite.execute(function() { _ready = true; });
+			      waitsFor(function() { return _ready; });
+			      runs(function() { fn(_suite); } );
+			  }
+			  function given_suite_with(fn) {
+			      _suite = _env.describe('suite', function() { fn(_env); } );
+			  }
+			  it('passes on holding properties', function() {
+				 given_suite_with(function(env){
+						 env.property('property that holds', qc.gen.integer, 
+							      function(i) {
+								  this.expect(_.isNumber(i)).toEqual(true);
+							      });
+					     });
+				 when_suite_has_been_run(function(suite) {
+							     expect(suite.results().passedCount).toEqual(1);
+							     expect(suite.results().totalCount).toEqual(1);
+							 });
+			     });
+			  
+			  it('fails when property does not hold', function(i) {
+				 given_suite_with(function(env) {
+						 env.property('property that fails with some value', qc.gen.integer,
+							      function(i) {
+								  this.expect(i).toBeLessThan(5);
+							      });
+					     });
+				 when_suite_has_been_run(function(suite) {
+							     expect(suite.results().failedCount).toEqual(1);
+							     expect(suite.results().totalCount).toEqual(1);
+							 });
+			     });
+			  
+			  it('allows specs and properties in the same suite', function() {
+				 given_suite_with(function(env) {
+						 env.it('spec', function() {
+						 	      this.expect(true).toBeTruthy();
+						 	  });
+ 						 env.property('property', qc.gen.integer,
+						 	      function(i) {
+						 		  this.expect(true).toBeTruthy();
+						 	      });
+ 						 env.property('failing property', qc.gen.integer,
+						 	      function(i) {
+						 		  this.expect(true).toBeFalsy();
+						 	      });
+					     });
+				 when_suite_has_been_run(function(suite) {
+							     expect(suite.results().passedCount).toEqual(2);
+							     expect(suite.results().failedCount).toEqual(1);
+							     expect(suite.results().totalCount).toEqual(3);
+							 });
 			     });
 		      });
 	 });
